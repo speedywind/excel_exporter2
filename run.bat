dc33a0a65bc550885ce8f065aea34eac1fdcb8ac
wangxinji <wangxinji@fantablade.com>
2021-09-17 21:53:16 +0800

    [fix] run.bat

--- run.bat
+++ run.bat
@@ -32,7 +32,7 @@ if !hasInput!A NEQ A (
 	python .\main.py -c .\excel_exporter\check_config.json -d .\configuration\
 )
 if exist ..\WaterGun\Assets\LuaFramework\Lua\properties (
-	copy output\client\lua\*.lua ..\WaterGun\Assets\LuaFramework\Lua\properties
+	copy output\client_zh_cn\lua\*.lua ..\WaterGun\Assets\LuaFramework\Lua\properties
 	echo "rsync to ..\WaterGun\Assets\LuaFramework\Lua\properties"
 )
 pause
